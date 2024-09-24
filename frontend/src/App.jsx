24aa5bc86497e2b39b6211fbd1489720d078c7c4
unknown <anmoltripathi610@gmail.com>
2024-09-24 18:15:37 +0530

    vercel1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import SignUp from './pages/SignUp.jsx';
-import SignIn from './pages/Signin.jsx';
 import Pay from './component/User/Pay.jsx';
 import Transaction from './component/User/Transaction.jsx';
 import Balance from './component/User/Balance.jsx';
@@ -11,6 +10,7 @@ import Account from './component/User/Account.jsx';
 import User from './pages/User.jsx';
 import Dashboard from './component/User/Dashboard.jsx';
 import Payment from './pages/Payment.jsx';
+import SignIn from './pages/SignIn.jsx';
 
 function App() {
   return (
