3ccb86804f31d104ffd2fc1733d1a29ba238aace
Anmol Tripathi <anmoltripathi610@gmail.com>
2025-11-07 00:04:18 +0530

    changes of backend

--- backend/db.js
+++ backend/db.js
@@ -1,96 +1,120 @@
 // backend/db.js
-const mongoose = require('mongoose');
-const { boolean } = require('zod');
+const mongoose = require("mongoose");
+const { boolean } = require("zod");
 
+mongoose.connect(process.env.mongoDbUrl);
 
-
-
-const transactionSchema = new mongoose.Schema({
+const transactionSchema = new mongoose.Schema(
+  {
     oldBalance: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     newBalance: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     transferWithPersonName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    transferWithPersonProfilePic : {
-        type : String,
+    transferWithPersonProfilePic: {
+      type: String,
     },
-    transferWithPersonId : {
-        type : String , 
-        required : true,
+    transferWithPersonId: {
+      type: String,
+      required: true,
     },
     transactionAmount: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     MoneySent: {
-        type: Boolean,
-        required: true,
+      type: Boolean,
+      required: true,
     },
     msgAttached: {
-        type: String,
-    }
-}, { timestamps: true })
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-// Create a Schema for Users
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        minLength: 3,
-        maxLength: 30
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 6
-    },
-    firstName: {
-        type: String,
-        required: true,
-        trim: true,
-        maxLength: 50
-    },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true,
-        maxLength: 50
-    },
-    profilePic : {
-        type : String,
-    },
-    transactions: [transactionSchema],
-}, { timestamps: true });
-
-const accountSchema = new mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    balance: {
-        type: Number,
-        required: true
-    }
+const chatSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  isAi: {
+    type: Boolean,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
+// Create a Schema for Users
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minLength: 3,
+      maxLength: 30,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50,
+    },
+    profilePic: {
+      type: String,
+    },
+    transactions: [transactionSchema],
+    aiChats: [chatSchema],
+  },
+  { timestamps: true }
+);
 
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
 
-const Account = mongoose.model('Account', accountSchema);
-const User = mongoose.model('User', userSchema);
-const Transaction = mongoose.model('Transaction', transactionSchema);
+const Account = mongoose.model("Account", accountSchema);
+const User = mongoose.model("User", userSchema);
+const Transaction = mongoose.model("Transaction", transactionSchema);
 
 module.exports = {
-    User,
-    Account,
-    Transaction,
+  User,
+  Account,
+  Transaction,
 };
--- backend/index.js
+++ backend/index.js
@@ -1,52 +1,22 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
-const mongoose = require('mongoose');
-require('dotenv').config({
-    path: './.env' 
+require("dotenv").config({
+  path: "./.env",
 });
+const app = express();
+const db = require("./db");
 
+const corsOptions = {
+  origin: true, // Allow all origins
+  credentials: true, // Allow credentials
+};
 
-
-
-app.use(cors({
-    origin: 'https://paytm-react-project-jgkr.vercel.app',
-    methods: 'GET,POST,PUT,DELETE'
-}));
-
-// app.options('*', cors());
-
+app.use(cors(corsOptions));
 app.use(express.json());
-
 
 const mainRouter = require("./routes");
 
-
 app.use("/api/v1", mainRouter);
-app.get("/", (req, res) => {
-    res.send("backend is running");
+app.listen(3000, function () {
+  console.log("backend is running ");
 });
-
-const connectDB = async()=>{
-    try{
-        const connectionInstance= await mongoose.connect(process.env.mongoDbUrl)
-        console.log(`\n db connected !! DB HOST : ${connectionInstance.connection.host}`);
-    }
-    catch (error){
-        console.log("MONGOOSE CONNECTION ERROR",error);
-        process.exit(1)
-    }
-}
-connectDB()
-.then(()=>{
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, function () {
-        console.log(`Backend is running on port ${PORT}`);
-    });
-})
-.catch((err)=>{
-    console.log("MONGO db connection failed !!!",err)
-})
-
-
-
